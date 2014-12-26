c35df38b43b651ca460579cdf317453942331e27
delcoker <delcoker3@hotmail.com>
2014-12-26 23:33:13 +0000

    Merge origin/new_branch_martha into messages_plus_hod_advisor_upload
    Conflicts:
    	models/adb.php

--- classes/adb.php
+++ classes/adb.php
@@ -5,13 +5,15 @@
  * date:
  * description: A root class for all manage classes. This class communicates with DB
  */
-define("DB_HOST", 'localhost');
-define("DB_NAME", 'mw_hw_tracker');
-define("DB_PORT", 3306);
+//define("DB_HOST", 'localhost');
+//define("DB_NAME", 'mw_hw_tracker');
+//define("DB_PORT", 3306);
 //define("DB_USER", "root");
 //define("DB_PWORD", "");
 
-
+define("DB_HOST", 'localhost');
+define("DB_NAME", 'csashesi_kingston-coker');
+define("DB_PORT", 3306);
 define("DB_USER", "csashesi_kc15");
 define("DB_PWORD", "db!a4df4d");
 
